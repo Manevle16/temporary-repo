04f68cd015e9e2d060c4a1e5c5a23d4fd64bfbb6
Matthew Nevle <Manevle16@gmail.com>
2020-04-20 05:38:46 -0500

    first commit

---
+++ precache-manifest.8df8948a40a466ec4d22c59d5d36e5f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8741e672a326ba8ac60c41437221355b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac8bf42c4839c9206fc0",
+    "url": "./static/css/2.c3d00763.chunk.css"
+  },
+  {
+    "revision": "79f59eb23303fd254558",
+    "url": "./static/css/main.e668cc5e.chunk.css"
+  },
+  {
+    "revision": "ac8bf42c4839c9206fc0",
+    "url": "./static/js/2.6f0a10a5.chunk.js"
+  },
+  {
+    "revision": "2250062ea477171d069d14a3eee9c045",
+    "url": "./static/js/2.6f0a10a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79f59eb23303fd254558",
+    "url": "./static/js/main.377e4367.chunk.js"
+  },
+  {
+    "revision": "707b558cfa00bb544d58",
+    "url": "./static/js/runtime-main.85129088.js"
+  },
+  {
+    "revision": "72879e3e58b3ddf07235ca0cfd81a7cf",
+    "url": "./static/media/Epics.72879e3e.png"
+  }
+]);
